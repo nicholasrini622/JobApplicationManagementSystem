011bef3d0f211caeb3579b048aaca57dfc4cb953
Nicholas Rini <nicholasrini622@gmail.com>
2026-07-04 21:40:20 -0400

    Added validation back as methods, finished rest of CRUD operations and custom follow-up. Sort and filter added

--- src/ApplicationService.java
+++ src/ApplicationService.java
@@ -1,5 +1,8 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ApplicationService {
     private ArrayList<JobApplication> applications;
     private int followUpLimit;
@@ -14,24 +17,24 @@ public class ApplicationService {
     }
 
     public boolean validateApplication(JobApplication application) {
-        if (application == null || application.getStatus() == null || application.getWorkStructure() == null ) {
+        if (application == null || application.getStatus() == null || application.getWorkStructure() == null) {
             return false;
         }
         if (application.getCompany() == null || application.getCompany().isBlank() || application.getPosition() == null || application.getPosition().isBlank()
-        || application.getLocation() == null || application.getLocation().isBlank()){
+                || application.getLocation() == null || application.getLocation().isBlank()) {
             return false;
         }
-        if (application.getSalary() < 0){
+        if (!validateSalary(application.getSalary())) {
             return false;
         }
-        if (application.getApplicationDate() == null || application.getApplicationDate().isAfter(LocalDate.now())){
+        if (!validateDate(application.getApplicationDate())) {
             return false;
         }
         return true;
     }
 
     public boolean addApplication(JobApplication application) {
-        if (!validateApplication(application) || isDuplicateApplication(application)){
+        if (!validateApplication(application) || isDuplicateApplication(application)) {
             return false;
         }
         application.setApplicationID(createApplicationID());
@@ -39,13 +42,21 @@ public class ApplicationService {
         applications.add(application);
         return true;
     }
-    public boolean updateApplication(JobApplication application){
-        if(!validateApplication(application)){
+
+    public boolean updateApplication(JobApplication application) {
+        if (!validateApplication(application)) {
             return false;
         }
         JobApplication currentApplication = findApplicationById(application.getApplicationID());
-        if(currentApplication == null){
+        if (currentApplication == null) {
             return false;
+        }
+        for (JobApplication app : applications) {
+            if (app.getApplicationID() != application.getApplicationID()) {
+                if (app.getCompany().equalsIgnoreCase(application.getCompany()) && app.getPosition().equalsIgnoreCase(application.getPosition())) {
+                    return false;
+                }
+            }
         }
         currentApplication.setCompany(application.getCompany());
         currentApplication.setPosition(application.getPosition());
@@ -57,38 +68,137 @@ public class ApplicationService {
         currentApplication.setLastUpdatedDate(LocalDate.now());
         return true;
     }
-    public JobApplication findApplicationById(int applicationID){
-        for(JobApplication application : applications){
-            if(application.getApplicationID() == applicationID){
+
+    public boolean removeApplication(int applicationID) {
+        JobApplication application = findApplicationById(applicationID);
+        if (application == null) {
+            return false;
+        }
+        applications.remove(application);
+        return true;
+    }
+
+    public JobApplication findApplicationById(int applicationID) {
+        for (JobApplication application : applications) {
+            if (application.getApplicationID() == applicationID) {
                 return application;
             }
         }
         return null;
     }
 
-    private int createApplicationID(){
+    public ArrayList<JobApplication> getAllApplications() {
+        return applications;
+    }
+
+    private int createApplicationID() {
         int maxID = 0;
-        for(JobApplication application : applications){
-            if(application.getApplicationID() > maxID){
+        for (JobApplication application : applications) {
+            if (application.getApplicationID() > maxID) {
                 maxID = application.getApplicationID();
             }
         }
         return maxID + 1;
     }
-    private boolean isDuplicateApplication(JobApplication applicationCopy){
-        for(JobApplication application : applications){
-            if(application.getCompany().equalsIgnoreCase(applicationCopy.getCompany()) && application.getPosition().equalsIgnoreCase(applicationCopy.getPosition())){
+
+    public ArrayList<JobApplication> sortApplications(String sortField) {
+        ArrayList<JobApplication> sortedList = new ArrayList<>(applications);
+        if (sortField == null || sortField.isBlank()) {
+            return sortedList;
+        }
+        switch (sortField.trim().toLowerCase()) {
+            case "company":
+                Collections.sort(sortedList, new Comparator<JobApplication>() {
+                    @Override
+                    public int compare(JobApplication a, JobApplication b) {
+                        return a.getCompany().compareToIgnoreCase(b.getCompany());
+                    }
+                });
+                break;
+
+            case "position":
+                Collections.sort(sortedList, new Comparator<JobApplication>() {
+                    @Override
+                    public int compare(JobApplication a, JobApplication b) {
+                        return a.getPosition().compareToIgnoreCase(b.getPosition());
+                    }
+                });
+                break;
+
+            case "salary":
+                Collections.sort(sortedList, new Comparator<JobApplication>() {
+                    @Override
+                    public int compare(JobApplication a, JobApplication b) {
+                        return Double.compare(b.getSalary(), a.getSalary());
+                    }
+                });
+                break;
+
+            case "date":
+                Collections.sort(sortedList, new Comparator<JobApplication>() {
+                    @Override
+                    public int compare(JobApplication a, JobApplication b) {
+                        return a.getApplicationDate().compareTo(b.getApplicationDate());
+                    }
+                });
+                break;
+            default:
+                break;
+        }
+        return sortedList;
+    }
+    public ArrayList<JobApplication> filterByStatus(ApplicationStatus status){
+        ArrayList<JobApplication> filterList = new ArrayList<>();
+        for(JobApplication application: applications){
+            if(application.getStatus() == status){
+                filterList.add(application);
+            }
+        }
+        return filterList;
+    }
+
+    private boolean isDuplicateApplication(JobApplication applicationCopy) {
+        for (JobApplication application : applications) {
+            if (application.getCompany().equalsIgnoreCase(applicationCopy.getCompany()) && application.getPosition().equalsIgnoreCase(applicationCopy.getPosition())) {
                 return true;
             }
         }
         return false;
     }
-    public boolean isFollowUpNeeded(JobApplication application){
-        if(application == null || application.getStatus() == ApplicationStatus.DENIED || application.getLastUpdatedDate() == null){
+
+    public boolean isFollowUpNeeded(JobApplication application) {
+        if (application == null || application.getStatus() == ApplicationStatus.DENIED || application.getLastUpdatedDate() == null) {
             return false;
         }
         LocalDate followUpDate = LocalDate.now().minusDays(followUpLimit);
 
         return !application.getLastUpdatedDate().isAfter(followUpDate);
     }
+    public ArrayList<JobApplication> getFollowUpApplications(){
+        ArrayList<JobApplication> followUp = new ArrayList<>();
+        for(JobApplication application : applications){
+            if(application != null){
+                if(isFollowUpNeeded(application)){
+                    application.setFollowUpNeeded(true);
+                    followUp.add(application);
+                }
+                else{
+                    application.setFollowUpNeeded(false);
+                }
+            }
+        }
+        return followUp;
+    }
+
+    public boolean validateSalary(double salary) {
+        return salary >= 0;
+    }
+
+    public boolean validateDate(LocalDate date) {
+        if (date == null) {
+            return false;
+        }
+        return !date.isAfter(LocalDate.now());
+    }
 }
+
